6873c83f95fee03877cad36c987ef0ec508a2119
Jakob Meng <code@jakobmeng.de>
2024-03-26 12:03:06 +0100

    Added instructions on how to set up dotfiles

--- .config/bash_aliases.d/dotfiles.bash
+++ .config/bash_aliases.d/dotfiles.bash
@@ -1,11 +1,31 @@
 # 2024 Jakob Meng, <jakobmeng@web.de>
 # vim:set fileformat=unix tabstop=8 shiftwidth=4 expandtab autoindent:
 # kate: end-of-line unix; space-indent on; indent-width 4;
-
-# Dotfiles Management
 #
-# Ref.:
-# [0] https://mitxela.com/projects/dotfiles_management
+# Dotfiles Management
+# Ref.: https://mitxela.com/projects/dotfiles_management
+
+# How to set up
+# -------------
+#
+# $> git clone --bare https://github.com/JM1/dotfiles.git ~/.config/dotfiles
+# $> alias dotfiles='git "--git-dir=$HOME/.config/dotfiles" "--work-tree=$HOME"'
+# $> dotfiles config --local status.showUntrackedFiles no
+#
+# Unstage changes
+# $> dotfiles restore --staged $HOME
+#
+# List changed files
+# $> dotfiles status
+#
+# Restore deleted files
+# $> dotfiles restore $(dotfiles ls-files -d)
+#
+# List changes for remaining files
+# $> dotfiles diff
+#
+# Overwrite changes to modified files
+# $> dotfiles restore $HOME
 
 alias dotfiles='git "--git-dir=$HOME/.config/dotfiles" "--work-tree=$HOME"'
 
